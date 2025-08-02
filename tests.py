458328920f11e4fa08859309691a450e73678355
Julia-koroleva <julia-koroleva@list.ru>
2025-08-02 18:31:52 +0300

    Twefth test

--- tests.py
+++ tests.py
@@ -116,3 +116,13 @@ class TestBooksCollector:
         books_with_specific_genre = collector.get_books_with_specific_genre(specific_genre)
         books_for_children = collector.get_books_for_children()
         assert len(books_for_children) != books_with_specific_genre
+
+
+
+class TestBooksCollector:
+    
+    def test_get_list_of_favorites_books_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Золотой ключик')
+        collector.add_book_in_favorites('Золотой ключик')
+        assert len(collector.get_list_of_favorites_books()) == 1
